1f70714ca803ce46aa7c13916cf15873f2828811
Jessica Vaughn <jessica.vaughn619@gmail.com>
2024-01-15 10:31:48 -0700

    Add useEffect to fetch data from api/beers route

--- src/app/Beerlist/beerlist.js
+++ src/app/Beerlist/beerlist.js
@@ -1,19 +1,39 @@
 'use client'
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import BeerCard from "./beercard"
-import mLogo from "../../../public/images/m-logo.jpeg"
+// import mLogo from "../../../public/images/m-logo.jpeg"
  
 export default function Beerlist() {
-  const [beers, setBeers] = useState([{
-    id: 1,
-    image: mLogo,
-    name: "Pils",
-    style: "Pilsner",
-    brewery: "Matteson Brewing",
-    description: "Delicious pilsner",
-    abv: 4,
-    ibu: 80
-  }])
+  const [beers, setBeers] = useState([
+  //   {
+  //   id: 1,
+  //   image: mLogo,
+  //   name: "Pils",
+  //   style: "Pilsner",
+  //   brewery: "Matteson Brewing",
+  //   description: "Delicious pilsner",
+  //   abv: 4,
+  //   ibu: 80
+  // }
+])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/beers');
+        if (response.ok) {
+          const responseData = await response.json();
+          setBeers(responseData);
+        } else {
+          console.error('Failed to fetch data');
+        }
+      } catch (err) {
+        console.err('An unexpected error occurred', err);
+      }
+    };
+    fetchData();
+  }, [])
+
   const allBeers = beers.map(beer => (
     <BeerCard 
     key={beer.id}
